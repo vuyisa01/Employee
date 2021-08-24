b969b872fe4d7e23bc19749598b9021f4251227c
vuyisa01 <218192215@mycput.ac.za>
2021-08-24 14:16:38 +0200

    first commit

---
+++ src/main/java/za/ac/cput/Entity/Employee.java
@@ -0,0 +1,54 @@
+package za.ac.cput.Entity;
+
+public class Employee {
+    private String employeeNumb, firstName, lastName;
+
+    private Employee(Builder builder) {
+        this.employeeNumb = builder.employeeNumb;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+    }
+    @Override
+    public String toString() {
+        return "Builder{" +
+                "employeeNumb='" + employeeNumb + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+    private String employeeNumb, firstName, lastName;
+
+    public Builder setEmployeeNumb(String employeeNumb){
+        this.employeeNumb = employeeNumb;
+
+        return this;
+    }
+
+    public Builder setFirstName(String firstName){
+        this.firstName = firstName;
+
+        return this;
+    }
+
+    private Builder setLastName(String lastName){
+        this.lastName = lastName;
+
+        return this;
+    }
+    public Employee build(){
+        return new Employee(this);
+    }
+    public Builder copy(Employee employee){
+        this.employeeNumb = employee.employeeNumb;
+        this.firstName = employee.firstName;
+        this.lastName = employee.lastName;
+
+        return this;
+    }
+
+
+    }
+
+}
